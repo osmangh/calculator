4574253df5166f8bf5b490eb5dcf5c164e4aebf5
osman <osmanghani2357@gmail.com>
2022-04-09 19:35:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('#one').click(function(){
+        document.forms.display.value +=1;
+    })
+    $('#two').click(function(){
+        document.forms.display.value +=2;
+    })
+    $('#three').click(function(){
+        document.forms.display.value +=3;
+    })
+    $('#four').click(function(){
+        document.forms.display.value +=4;
+    })
+    $('#five').click(function(){
+        document.forms.display.value +=5;
+    })
+    $('#six').click(function(){
+        document.forms.display.value +=6;
+    })
+    $('#seven').click(function(){
+        document.forms.display.value +=7;
+    })
+    $('#eight').click(function(){
+        document.forms.display.value +=8;
+    })
+    $('#nine').click(function(){
+        document.forms.display.value +=9;
+    })
+    $('#add').click(function(){
+        document.forms.display.value +='+';
+    })
+    $('#zero').click(function(){
+        document.forms.display.value +=0;
+    })
+    $('#minus').click(function(){
+        document.forms.display.value +='-';
+    })
+    $('#dot').click(function(){
+        document.forms.display.value +='.';
+    })
+    $('#mul').click(function(){
+        document.forms.display.value +='*';
+    })
+    $('#dive').click(function(){
+        document.forms.display.value +='/';
+    })
+    $('#equal').click(function(){
+        if(display==""){
+            alert('write something')
+        }
+        else{
+            document.forms.display.value=eval(document.forms.display.value);
+        }
+    })
+    $('#clear').click(function(){
+        document.forms.display.value="";
+    })
+})
